453ca93960a37826e0b59ff157b7699ee848705a
bartmao <maochen.mars@qq.com>
2017-09-06 23:13:37 +0800

    init

---
+++ Excel数据分类整理工具/Form1.cs
@@ -0,0 +1,42 @@
+﻿using NPOI.HSSF.UserModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Excel数据分类整理工具
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (var stream = File.OpenRead("")) {
+                var workbook = new HSSFWorkbook(stream);
+                for (int i = 0; i < workbook.NumberOfSheets; i++)
+                {
+                    var sheet = workbook.GetSheetAt(i);
+                    if (Regex.IsMatch(sheet.SheetName, @"[\d|\.]+")) {
+                        for (int j = 5; j < sheet.LastRowNum; j++)
+                        {
+                            var row = sheet.GetRow(j);
+                            var designation = row.GetCell(2);
+                        }
+                    }
+                }
+            }
+            
+        }
+    }
+}
---
+++ Excel数据分类整理工具/Item.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excel数据分类整理工具
+{
+    public class CItem
+    {
+        public CCell Designation { get; set; }
+
+        public CCell[] Attributes { get; set; }
+
+        public CCell CS { get; set; }
+
+        public CCell RMB { get; set; }
+
+    }
+
+    public class CCell {
+        public string Position { get; set; }
+
+        public string Value { get; set; }
+    }
+}
